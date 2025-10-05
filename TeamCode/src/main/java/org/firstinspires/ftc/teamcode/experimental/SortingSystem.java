28339cde595ff1405e931de4b23d44892fb0f706
pellathyse-prog <pellathyse@wilmingtonfriends.org>
2025-10-05 12:56:49 -0400

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/experimental/SortingSystem.java
@@ -0,0 +1,62 @@
+package com.example;
+
+public class SortingSystem {
+    public static void main(String[] args) {
+        int[] array = {0, 1, 0};
+
+        int purplecount = 0;
+        int greencount = 0;
+        // 0 for purple, 1 for green
+
+    }
+
+
+    public static int detectColor() {
+        // Dummy implementation for color detection
+        return 0; // Assume it detects purple
+    }
+
+    public static void eject(int color) {
+        if (color == 0) {
+            // Eject purple item
+            --purplecount;
+        } else {
+            // Eject green item
+            --greencount;
+        }
+    }
+
+    public static void distribute() {
+        if (detectColor() == 0) {
+            // Move into left chamber
+            ++purplecount;
+        } else {
+            // Move into right chamber
+            ++greencount;
+        }
+    }
+
+    public static void fire() {
+        // navigate to firing position
+        for (int i = 0; i < 3; i++) {
+            eject(array[i]);
+        }
+    }
+
+    public static void sort() {
+        if (detectColor() == 0) {
+            if (purplecount > 2) {
+                eject(0);
+            }
+            distribute();
+        }
+        else {
+            if (greencount > 1) {
+                eject(1);
+            }
+            distribute();
+        }
+    }
+
+
+}
